e7f7b6fadcde4009e0cdd56e8fcf156948c563ca
MM696 <Mpiremovies@gmail.com>
2025-07-13 00:07:45 +0100

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,9 @@ import React, { useState } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import SignUpForm from "./pages/signupform";
 import LoginForm from "./pages/LoginForm";
-<<<<<<< HEAD
-// import Dashboard from "./pages/Dashboard";
-=======
 import Dashboard from "./pages/Dashboard";
 import Symptoms from "./pages/Symptoms";
 import Result from "./pages/Resultpage"; // make sure this is the right path
->>>>>>> 161c345ecfe4c2ff1a82084368937d39ec216d26
 import "./App.css";
 
 function App() {
@@ -22,9 +18,6 @@ function App() {
         <Route path="/" element={<Navigate to="/signup" />} />
         <Route path="/signup" element={<SignUpForm />} />
         <Route path="/login" element={<LoginForm />} />
-<<<<<<< HEAD
-        {/* <Route path="/dashboard" element={<Dashboard />} /> */}
-=======
         <Route path="/dashboard" element={<Dashboard />} />
 
         {/* Pass props correctly to Symptoms page */}
@@ -50,7 +43,6 @@ function App() {
             />
           }
         />
->>>>>>> 161c345ecfe4c2ff1a82084368937d39ec216d26
       </Routes>
     </BrowserRouter>
   );
