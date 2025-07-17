8dde675b553a14860ea4601e2f05c0da8a93f59f
MM696 <Mpiremovies@gmail.com>
2025-07-17 10:16:06 +0100

    removed base configuration line

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/', // 👈 This must match the basename in BrowserRouter
   plugins: [react()],
 })
