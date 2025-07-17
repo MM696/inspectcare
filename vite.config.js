cec501ccd1a928f170557efa4962c2df94bfc88e
MM696 <Mpiremovies@gmail.com>
2025-07-17 09:59:51 +0100

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/flexisaf-inspectcare/', // 👈 This must match the basename in BrowserRouter
+  base: '/', // 👈 This must match the basename in BrowserRouter
   plugins: [react()],
 })
